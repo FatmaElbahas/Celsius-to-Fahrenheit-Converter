24bd6aeb490c327ab02f705f1ff51df2eb64508d
PASSWOED <PASSWOED@DESKTOP-UI80RJC>
2024-12-07 23:54:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp106
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The Temprerature In Celsuis");
+            double celsuis=Convert.ToDouble(Console.ReadLine());
+            Double fahrenheit = (celsuis * 9 / 5) + 32;
+            Console.WriteLine("celsuis " + celsuis);
+            Console.WriteLine("fahrenheit " + fahrenheit);
+
+        }
+    }
+}
